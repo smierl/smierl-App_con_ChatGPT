59602315fbf11e4a02f1eeef5d5c5ab258cab5bd
smierl <145398729+smierl@users.noreply.github.com>
2024-02-12 16:32:52 -0500

    Update mi_primera_app.py

--- mi_primera_app.py
+++ mi_primera_app.py
@@ -1,13 +1,16 @@
+# Importar el módulo de Streamlit
 import streamlit as st
 
-# Título y autor
+# Título de la app
 st.title("Mi primera app")
-st.write("Esta app fue elaborada por Gabriel AWAD.")
 
-# Preguntar el nombre al usuario
-nombre_usuario = st.text_input("Ingresa tu nombre")
+# Autor de la app
+st.write("Esta app fue elaborada por COLOQUE AQUÍ SU NOMBRE")
 
-# Imprimir el mensaje de bienvenida
+# Preguntar al usuario su nombre
+nombre_usuario = st.text_input("Por favor, ingresa tu nombre")
+
+# Verificar si se ingresó un nombre
 if nombre_usuario:
-    mensaje = f"{nombre_usuario}, te doy la bienvenida a mi primera app."
-    st.write(mensaje)
+    # Mostrar mensaje de bienvenida
+    st.write(f"{nombre_usuario}, te doy la bienvenida a mi primera app.")
