e6256e3a08598bec9aab46e9c2716ae18fb77073
smierl <145398729+smierl@users.noreply.github.com>
2024-02-12 16:46:26 -0500

    Update appConversion.py

--- appConversion.py
+++ appConversion.py
@@ -22,6 +22,26 @@ def main():
 
     st.write(f"Has seleccionado: {seleccion_conversion}")
 
+    # Diccionario con las opciones de conversión
+    opciones_por_conversion = {
+        "Conversiones de temperatura": ["Celsius a Fahrenheit", "Fahrenheit a Celsius", "Celsius a Kelvin", "Kelvin a Celsius"],
+        "Conversiones de longitud": ["Pies a metros", "Metros a pies", "Pulgadas a centímetros", "Centímetros a pulgadas"],
+        "Conversiones de peso/masa": ["Libras a kilogramos", "Kilogramos a libras", "Onzas a gramos", "Gramos a onzas"],
+        "Conversiones de volumen": ["Galones a litros", "Litros a galones", "Pulgadas cúbicas a centímetros cúbicos", "Centímetros cúbicos a pulgadas cúbicas"],
+        "Conversiones de tiempo": ["Horas a minutos", "Minutos a segundos", "Días a horas", "Semanas a días"],
+        "Conversiones de velocidad": ["Millas por hora a kilómetros por hora", "Kilómetros por hora a metros por segundo", "Nudos a millas por hora", "Metros por segundo a pies por segundo"],
+        "Conversiones de área": ["Metros cuadrados a pies cuadrados", "Pies cuadrados a metros cuadrados", "Kilómetros cuadrados a millas cuadradas", "Millas cuadradas a kilómetros cuadrados"],
+        "Conversiones de energía": ["Julios a calorías", "Calorías a kilojulios", "Kilovatios-hora a megajulios", "Megajulios a kilovatios-hora"],
+        "Conversiones de presión": ["Pascales a atmósferas", "Atmósferas a pascales", "Barras a libras por pulgada cuadrada", "Libras por pulgada cuadrada a bares"],
+        "Conversiones de tamaño de datos": ["Megabytes a gigabytes", "Gigabytes a Terabytes", "Kilobytes a megabytes", "Terabytes a petabytes"]
+    }
+
+    # Desplegar las opciones de conversión correspondientes
+    opciones_seleccionadas = opciones_por_conversion.get(seleccion_conversion, [])
+    seleccion_conversion_secundaria = st.selectbox("Selecciona la conversión específica:", opciones_seleccionadas)
+
+    st.write(f"Has seleccionado: {seleccion_conversion_secundaria}")
+
 if __name__ == "__main__":
     main()
 
